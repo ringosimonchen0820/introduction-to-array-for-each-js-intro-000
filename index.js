5ddf94745efd70646f00f231c216ec2dc7aedb52
Shu Fu <ringosimonchen0820@gmail.com>
2019-08-23 20:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Marh.random() * 100 + 2).toString() + "${array[index]}s!!!";
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
+console.log(animals);
